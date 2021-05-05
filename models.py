d4115051bafbf74d744fc8c2d8c223887b9f032a
Saad Shamim <saadshamim01@gmail.com>
2021-05-05 10:23:07 +0400

    - Final

--- models.py
+++ models.py
@@ -21,65 +21,65 @@ def setup_db(app, database_path=database_path):
     db.create_all()
     #migrate = Migrate(app,db)
 
-#class Actor(db.Model):
-#    __tablename__ = 'Actor'
-#    id = db.Column(db.Integer, primary_key=True)
-#    name = db.Column(db.String)
-#    age = db.Column(db.Integer)
-#    gender = db.Column(db.String)
-#
-#    def __init__(self, name, age, gender):
-#        self.name = name
-#        self.age = age
-#        self.gender = gender
-#
-#    def insert(self):
-#        db.session.add(self)
-#        db.session.commit()
-#
-#    def update(self):
-#        db.session.commit()
-#
-#    def delete(self):
-#        db.session.delete(self)
-#        db.session.commit()
-#
-#    def format(self):
-#        return {
-#        'id': self.id,
-#        'name': self.name,
-#        'age': self.age,
-#        'gender': self.gender
-#        }
-#    def __repr__(self):
-#        return f'<Actors ID: {self.id} Name: {self.name} Age: {self.age} Gender: {self.gender}>'
-#class Movie(db.Model):
-#    __tablename__ = 'Movie'
-#    id = db.Column(db.Integer, primary_key=True)
-#    title = db.Column(db.String(120))
-#    release_date = db.Column(db.DateTime)
-#
-#    def __init__(self, title, release_date):
-#        self.title = title
-#        self.release_date
-#
-#    def insert(self):
-#        db.session.add(self)
-#        db.session.commit()
-#
-#    def update(self):
-#        db.session.commit()
-#
-#    def delete(self):
-#        db.session.delete(self)
-#        db.session.commit()
-#
-#    def format(self):
-#        return{
-#        'id': self.id,
-#        'title': self.title,
-#        'release_date': self.release_date
-#        }
+class Actor(db.Model):
+    __tablename__ = 'Actor'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    age = db.Column(db.Integer)
+    gender = db.Column(db.String)
+
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+        'id': self.id,
+        'name': self.name,
+        'age': self.age,
+        'gender': self.gender
+        }
+##    def __repr__(self):
+##        return f'<Actors ID: {self.id} Name: {self.name} Age: {self.age} Gender: {self.gender}>'
+class Movie(db.Model):
+    __tablename__ = 'Movie'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120))
+    release_date = db.Column(db.DateTime)
+
+    def __init__(self, title, release_date):
+        self.title = title
+        self.release_date
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return{
+        'id': self.id,
+        'title': self.title,
+        'release_date': self.release_date
+        }
 
 #    def __repr__(self):
 #        return f'<Movies ID: {self.id} Title: {self.title} release_date: {self.release_date}>'
